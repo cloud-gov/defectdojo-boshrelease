012231c0c5b29d8d3dd33d4d24ec2ff1c587c52d
James Hochadel <james.hochadel@gsa.gov>
2023-08-28 16:38:23 -0400

    Extract Defect Dojo source to release src dir

--- ci/process-release.sh
+++ ci/process-release.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Start from clean src folder
 rm -rf src/src/*
-tar -xzvf release/source.tar.gz --directory src/src
+
+# BOSH release source code belongs in src directory
+tar -xzf release/source.tar.gz --directory src/src
+
+# Directory name changes with every release, so make it constant
+ls src/src | xargs --replace=% mv % DefectDojo
+
+# Create a topic branch named after the new version
+git checkout -b defectdojo-$(cat release/version)
+
 # bosh add-blob --dir=src release/source.tar.gz defectdojo/source.tar.gz
 exit 1
