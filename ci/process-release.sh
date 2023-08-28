ebdedfe90f31eaf41c3bebc3dfca1022cfec3b9c
James Hochadel <james.hochadel@gsa.gov>
2023-08-28 16:18:24 -0400

    Forgot flag

--- ci/process-release.sh
+++ ci/process-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf src/src/*
-tar -xzvf release/source.tar.gz src/src
+tar -xzvf release/source.tar.gz --directory src/src
 # bosh add-blob --dir=src release/source.tar.gz defectdojo/source.tar.gz
 exit 1
