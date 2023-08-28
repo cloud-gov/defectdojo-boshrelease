b158762e4f938043c1176a44c45c058ef6bf576d
James Hochadel <james.hochadel@gsa.gov>
2023-08-28 18:18:01 -0400

    Commit and push changes and create a PR using cg-ci-bot

--- ci/process-release.sh
+++ ci/process-release.sh
@@ -9,9 +9,23 @@ tar -xzf release/source.tar.gz --directory src/src
 # Directory name changes with every release, so make it constant
 ls src/src | xargs --replace=% mv src/src/% src/src/DefectDojo
 
+DEFECTDOJO_VERSION=$(cat ../release/version)
+
 # Create a topic branch named after the new version
 cd src
-git checkout -b defectdojo-$(cat ../release/version)
+git checkout -b defectdojo-$DEFECTDOJO_VERSION
+
+# Commit changes
+git add src
+git commit -m "Bump Defect Dojo to $DEFECTDOJO_VERSION"
+
+# Push and PR
+git push -u origin $(git branch --show-current)
+# Token set as GH_TOKEN is automatically used. See `gh help environment`.
+gh pr create \
+	--base main \
+	--fill \
+	--reviewer "@cloud-gov/platform-ops"
 
 # bosh add-blob --dir=src release/source.tar.gz defectdojo/source.tar.gz
 exit 1
